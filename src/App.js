c36563ee1090635d5437526184c5003d8dbef3e9
Yingying SUN <yingying.sun7@hotmail.com>
2022-05-18 10:41:48 +1000

    柴柴课程项目第一次提交

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { unstable_HistoryRouter as Router, Routes, Route } from 'react-router-dom'
+import { history } from './utils/history'
+import { AuthComponent } from "./components/AuthComponent"
+import './App.css'
+import { lazy, Suspense } from 'react'
 
-function App() {
+//使用路由懒加载，下面这些原来引入的组件要改写
+// import Home from './pages/home'
+// import Article from './pages/article'
+// import Publish from './pages/publish'
+// import Login from '@/pages/login'
+// import LayoutPage from '@/pages/layout'
+
+// 按需导入路由组件
+const Login = lazy(() => import('./pages/login'))
+const LayoutPage = lazy(() => import('./pages/layout'))
+const Home = lazy(() => import('./pages/home'))
+const Article = lazy(() => import('./pages/article'))
+const Publish = lazy(() => import('./pages/publish'))
+
+
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    //路由配置
+    <Router history={history}>
+      <div className="App">
+        <Suspense    //使用 Suspense 组件包裹组件内容
+          fallback={  //为 Suspense 组件提供 fallback 属性，指定 loading 占位内容
+            <div
+              style={{
+                textAlign: 'center',
+                marginTop: 200
+              }}
+            >
+              loading...
+            </div>
+          }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <Routes>
+            {/* Layout需要用{ AuthComponent }来进行鉴权处理，要根据登录状态来判断是否渲染 */}
+            <Route path='/' element={
+              <AuthComponent>
+                <LayoutPage />
+              </AuthComponent>
+            }>
+              <Route index element={<Home />}></Route>
+              <Route path='/article' element={<Article />}></Route>
+              <Route path='/publish' element={<Publish />}></Route>
+            </Route>
+            <Route path='/login' element={<Login />}></Route>
+          </Routes>
+        </Suspense>
+      </div>
+    </Router>
+  )
 }
 
-export default App;
+export default App
